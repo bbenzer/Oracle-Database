a9f3421ee5d17a9d8f5118b4ecf28c2db3950498
bbenzer <berk.benzer@stu.bahcesehir.edu.tr>
2019-06-19 11:04:08 +0300

    Update ora_errors.sql

--- ora_errors.sql
+++ ora_errors.sql
@@ -30,3 +30,21 @@ ORA-01950: no privileges on tablespace "Users"
 alter user test quota unlimited on USERS;
 
 -----------------------------
+
+
+/*
+
+ORA-08189: Cannot Flashback The Table Because Row Movement Is Not Enabled
+
+
+*/
+
+flashback table ORDERS.test  to timestamp systimestamp - interval '1' hour;
+
+select table_name,ROW_MOVEMENT from dba_tables where table_name='TEST'
+
+ALTER TABLE ORDERS.TEST ENABLE ROW MOVEMENT ;
+
+
+
+
